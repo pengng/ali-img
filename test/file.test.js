caf873c4984eace44d0e680c87ced931f00ff6b3
yupeng.kuo <yupeng.kuo@gmail.com>
2017-09-14 00:51:41 +0800

    update

--- test/file.test.js
+++ test/file.test.js
@@ -9,16 +9,22 @@ const img = new AliImg({
 })
 
 describe('test lib/file.js', function () {
-  it('test getMD5()', function () {
-    const md5str = img.getMD5('Hello World')
-    assert.equal(md5str, 'sQqNsWTgdUEFt6mb5y4/5Q==')
-  })
-  it('test getObject()', function (done) {
-    img.getObject('google.png', function (err, response, body) {
-      if (err) {
-        return console.error(err)
-      }
-      console.log(body)
+  // it('test getMD5()', function () {
+  //   const md5str = img.getMD5('Hello World')
+  //   assert.equal(md5str, 'sQqNsWTgdUEFt6mb5y4/5Q==')
+  // })
+  // it('test getObject()', function (done) {
+  //   img.getObject('google.png', function (err, response, body) {
+  //     if (err) {
+  //       return console.error(err)
+  //     }
+  //     console.log(body)
+  //     done()
+  //   })
+  // })
+  it('test putObject()', function (done) {
+    img.putObjectFromUrl('http://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLmnGMszeibYjRDarwbbSErz3oTQZ7PJLVCzIBBA1NwoONXjf5wIVOl223iaTia9BVQkLrCOL6IGk42Q/0', 'google4.png', function (err, response, body) {
+      console.log(arguments)
       done()
     })
   })
